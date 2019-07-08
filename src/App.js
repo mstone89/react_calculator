9073cab3561e0771e2abf2a76d83aaa2d0c340c5
Molly Stone <molly.stone89@gmail.com>
2019-07-08 15:22:09 -0400

    Add additional basic styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Buttons from './components/Buttons';
 
 const CalculatorContainer = styled.div `
     width: 400px;
+    border: 5px solid darkgray;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: lightgray;
 `
 
 const StyledDisplay = styled.div `
     font-size: 50px;
+    color: white;
     width: 400px;
     text-align: right;
+    background-color: cornflowerblue;
+    border-radius: 5px;
 `
 
 class App extends Component {
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.div `
-    font-size: 20px;
-    width: 100px;
+    font-size: 30px;
+    width: 98px;
+    height: 50px;
     text-align: center;
+    border: 1px solid gray;
 `
 
 class Button extends Component {
