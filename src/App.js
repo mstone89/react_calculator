58f8054c4013dccdf29e08718a9189dc8570b5e1
Molly Stone <molly.stone89@gmail.com>
2019-07-08 20:01:21 -0400

    Break out clear display into separate function and able to toggle positive/negative ints

--- src/App.js
+++ src/App.js
@@ -29,7 +29,23 @@ class App extends Component {
             value: 0
         }
         this.determineAction = this.determineAction.bind(this);
+        this.setValue = this.setValue.bind(this);
         this.clearDisplay = this.clearDisplay.bind(this);
+        this.togglePosNeg = this.togglePosNeg.bind(this);
+    }
+
+    setValue(button) {
+        this.setState((currentState) => {
+            if (this.state.value > 0) {
+                return {
+                    value: currentState.value * 10 + button
+                }
+            } else {
+                return {
+                    value: parseInt(currentState.value.toString() + button)
+                }
+            }
+        });
     }
 
     clearDisplay() {
@@ -38,16 +54,21 @@ class App extends Component {
         });
     }
 
+    togglePosNeg() {
+        this.setState({
+            value: -(this.state.value)
+        });
+    }
+
     determineAction(button) {
         if (typeof button === 'number') {
-            this.setState((currentState) => {
-                return {
-                    value: currentState.value * 10 + button
-                }
-            });
+            this.setValue(button);
         }
         if (button === 'AC') {
             this.clearDisplay();
+        }
+        if (button === '+/-') {
+            this.togglePosNeg();
         }
     }
 
